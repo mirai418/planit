01bd0d4a5d549042c21f7eacc0db8d3e5e152c26
Willy Hauser <willyh258@gmail.com>
2013-01-19 21:22:12 -0500

    basic user logic

--- planit/app/controllers/users_controller.rb
+++ planit/app/controllers/users_controller.rb
@@ -40,15 +40,26 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(params[:user])
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+    if (@user = User.find_by_cell(params[:user][:cell]))
+      respond_to do |format|
+        if @user.update_attributes(params[:user])
+          format.html { redirect_to @user, notice: 'RSVP\'d successfully!' }
+          format.json { head :no_content }
+        else
+          format.html { render action: "edit" }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
+      end
+    else
+      @user = User.new(params[:user])
+      respond_to do |format|
+        if @user.save
+          format.html { redirect_to @user, notice: 'RSVP\'d successfully!' }
+          format.json { render json: @user, status: :created, location: @user }
+        else
+          format.html { render action: "new" }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
--- planit/app/models/user.rb
+++ planit/app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   attr_accessible :cell, :event_id, :gender, :name, :preference
 
   belongs_to :event
+
+  validates_presence_of :name, :event_id
 end
